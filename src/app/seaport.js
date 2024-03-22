8b1bebc64a106f4a6112027a2c6c315fb2bedc91
abhi152003 <abhiprajapati011@gmail.com>
2024-03-22 18:02:00 +0530

    changes in seaport.js

--- src/app/seaport.js
+++ src/app/seaport.js
@@ -3,28 +3,32 @@
 import { Seaport } from "@opensea/seaport-js";
 import { ethers } from "ethers";
 import { ItemType } from "@opensea/seaport-js/lib/constants";
+import { useState } from "react";
 
 const SeaportIntegration = () => {
-  const handleButtonClick = async () => {
+  const [order, setOrder] = useState(null);
+
+  const handleSellerClick = async () => {
     try {
       const provider = new ethers.BrowserProvider(window.ethereum);
       const seaport = new Seaport(provider);
 
       const signer = provider.getSigner();
-      console.log((await signer).getAddress)
+      // console.log((await signer).getAddress)
       const accountAddress = await signer;
 
       const {address} = accountAddress
+      console.log('addressssss', address);
 
-      const offerer = "0x160BCEdf12B4eE894a8f0faE0156e99f533ad534";
-      const fulfiller = "0xB5204aff106dc1Ffc6bE909c94a6A933081dB636";
+      const offerer = address;
+      const fulfiller = "0x21F44B7EF8AC550372bfbAb467F6Ad314717e10D";
       const { executeAllActions } = await seaport.createOrder(
         {
           offer: [
             {
-              itemType: ItemType.ERC721,
-              token: "0x931F2B0c88ca40975C379d1147CB142C6034716c",
-              identifier: "5",
+              itemType: ItemType.ERC1155,
+              token: "0x45cc23c87767Bb1B04E916EEc2b7ccbD82c90a9d",
+              identifier: "0",
             },
           ],
           consideration: [
@@ -38,13 +42,47 @@ const SeaportIntegration = () => {
       );
 
       const order = await executeAllActions();
+      setOrder(order);
+      console.log("adresss::::::::::::: ",address)
+
+      // const { executeAllActions: executeAllFulfillActions } =
+      //   await seaport.fulfillOrder({
+      //     order,
+      //     accountAddress: fulfiller,
+      //   });
+
+      // const transaction = executeAllFulfillActions();
+      console.log("Order:", order);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleBuyerClick = async () => {
+    try {
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const seaport = new Seaport(provider);
+
+      const signer = provider.getSigner();
+      // console.log((await signer).getAddress)
+      const accountAddress = await signer;
+
+      const {address} = accountAddress
+      console.log('addressssss', address);
+
+      // const order = await executeAllActions();
 
       console.log("adresss::::::::::::: ",address)
+
+      if (!order) {
+        console.error("No order available. Please create an order first.");
+        return;
+      }
 
       const { executeAllActions: executeAllFulfillActions } =
         await seaport.fulfillOrder({
           order,
-          accountAddress: fulfiller,
+          accountAddress: address,
         });
 
       const transaction = executeAllFulfillActions();
@@ -52,11 +90,12 @@ const SeaportIntegration = () => {
     } catch (error) {
       console.error("Error:", error);
     }
-  };
+  }
 
   return (
     <div>
-      <button onClick={handleButtonClick}>Execute Seaport Order</button>
+      <button onClick={handleSellerClick}>Seller</button>
+      <button onClick={handleBuyerClick}>Buyer</button>
     </div>
   );
 };
